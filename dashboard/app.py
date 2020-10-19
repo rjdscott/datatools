1fad718990dde31f2f0f527fcec32c7559d5882d
rjdscott <rob@rjdscott.com>
2020-10-18 20:44:50 -0400

    refactor

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,7 +4,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import plotly.express as px
-import plotly.figure_factory as ff
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -18,15 +17,18 @@ app.layout = html.Div([
             id='input-1-state',
             options=[
                 {'label': 'AAPL', 'value': 'AAPL'},
+                {'label': 'AMZN', 'value': 'AMZN'},
                 {'label': 'FB', 'value': 'FB'},
-                {'label': 'IBM', 'value': 'IBM'}
+                {'label': 'IBM', 'value': 'IBM'},
+                {'label': 'MSFT', 'value': 'MSFT'}
             ],
             value='AAPL',
             style=dict(
                 width='150px',
                 display='inline-block',
                 verticalAlign="middle")
-        )]),
+        )]
+    ),
     html.Div([
         html.Button(
             id='submit-button-state',
@@ -38,7 +40,6 @@ app.layout = html.Div([
             children='Submit')
     ]),
     html.Div(id='output-state')
-
 ])
 
 
@@ -46,17 +47,26 @@ app.layout = html.Div([
               [Input('submit-button-state', 'n_clicks')],
               [State('input-1-state', 'value')])
 def update_output(n_clicks, input1):
-    df = pd.read_parquet('../data/etl.parquet', columns=['ticker', 'date', 'adj_close', 'pct_chg_1d'])
+    """
+    Callback function for Dashboard
+    :param n_clicks: Button click counter
+    :param input1: Value of the dropdown
+    :return: dash html.Div() with graphs
+    """
 
+    # use pandas to read the parquet file from data dir,
+    # todo: update this to call API
+    df = pd.read_parquet('../data/etl.parquet', columns=['ticker', 'date', 'adj_close', 'pct_chg_1d'])
     df = df[df.ticker == input1]
 
+    # construct figures for dash output
     fig_line = px.line(df, x="date", y="adj_close", title=f'{input1} Stock History')
     fig_hist = px.histogram(df, x="pct_chg_1d", nbins=300, marginal="box", title=f'{input1}Returns Distribution')
 
+    # construct output object
     output = html.Div([
         dcc.Graph(id='example-graph-1', figure=fig_line),
         dcc.Graph(id='example-graph-2', figure=fig_hist),
-
     ])
 
     return output
