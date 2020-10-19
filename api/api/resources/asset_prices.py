b6c34459b14a98c3fa723d04cca3b3177a5e2f83
Rob Scott <rob@rjdscott.com>
2020-10-19 12:11:08 -0400

    Refactor (#7)
    * refactor asset price resource and add doc strings
    
    * tidy repo
    
    * tested with docker

--- api/api/resources/asset_prices.py
+++ api/api/resources/asset_prices.py
@@ -6,19 +6,37 @@ from datetime import datetime as dt
 import dask.dataframe as dd
 
 
-def get_ticker_data_dask(ticker, file_path):
+def get_ticker_data_dask(ticker: str, file_path: str) -> pd.DataFrame():
+    """
+    Get ticker data using Dask
+    :param ticker: string -> ticker for hist data
+    :param file_path: string -> path to parquet file
+    :return: DataFrame
+    """
     df = dd.read_parquet(file_path, columns=['ticker', 'date', 'adj_close', 'volume', 'pct_chg_1d'])
     df = df[df.ticker == ticker]
     return df.compute()
 
 
-def get_ticker_data_pandas(ticker, file_path):
+def get_ticker_data_pandas(ticker: str, file_path: str) -> pd.DataFrame():
+    """
+    Get ticker data using Pandas
+    :param ticker: string -> ticker for hist data
+    :param file_path: string -> path to parquet file
+    :return: DataFrame
+    """
     df = pd.read_parquet(file_path, columns=['ticker', 'date', 'adj_close', 'volume', 'pct_chg_1d'])
     df = df[df.ticker == ticker]
     return df
 
 
-def asset_price_message_from_df(ticker, prices_df):
+def asset_price_message_from_df(ticker: str, prices_df: pd.DataFrame()) -> ap.Asset():
+    """
+    Converts a pricing DataFrame to a Protobuf Message
+    :param ticker: string -> stock ticker
+    :param prices_df: DataFrame -> stock prices
+    :return: ap.Asset() message
+    """
     asset = ap.Asset()
     asset.symbol = ticker
 
@@ -31,41 +49,50 @@ def asset_price_message_from_df(ticker, prices_df):
     return asset
 
 
-def asset_message_from_df(ticker, prices_df):
-    asset = ap.Asset()
-    asset.symbol = ticker
-
-    for ix, row in prices_df.iterrows():
-        asset_price = asset.price.add()
-        asset_price.date = dt.strftime(dt.strptime(row['Date'], '%m/%d/%y'), '%Y-%m-%d')
-        asset_price.open = row['Open']
-        asset_price.high = row['High']
-        asset_price.low = row['Low']
-        asset_price.close = row['Close']
-        asset_price.close_adj = row['Adj Close']
-        asset_price.volume = row['Volume']
-
-    return asset
-
-
 def message_to_bytes(message):
+    """
+    Converts an ap.Asset() message to byte string
+    :param message: ap.Asset()
+    :return: string -> byte message
+    """
     return message.SerializeToString()
 
 
 def message_to_json(message):
+    """
+    Converts an ap.Asset() message to json string
+    :param message: ap.Asset()
+    :return: string -> json message
+    """
     return MessageToJson(message, preserving_proto_field_name=True)
 
 
 def message_to_dict(message):
+    """
+    Converts an ap.Asset() message to dictionary
+    :param message: ap.Asset()
+    :return: dict() -> message objects
+    """
     return MessageToDict(message, preserving_proto_field_name=True)
 
 
 def message_bytes_to_disk(message_bytes, file_path):
+    """
+    Writes byte string to local dick
+    :param message_bytes: string -> byte string
+    :param file_path: string -> path to disk
+    :return: None
+    """
     with open(file_path, 'wb') as f:
         f.write(message_bytes)
 
 
 def byte_file_to_message(file_path):
+    """
+    Reads bytes on disk and returns ap.Asset() message
+    :param file_path: string -> path to disk
+    :return: ap.Asset()
+    """
     asset = ap.Asset()
     with open(file_path, "rb") as f:
         asset.ParseFromString(f.read())
@@ -73,12 +100,25 @@ def byte_file_to_message(file_path):
 
 
 def byte_to_message(bytes_raw):
+    """
+    Converts byte string to message
+    :param bytes_raw: string - bytes
+    :return: ap.Asset()
+    """
     asset = ap.Asset()
     asset.ParseFromString(bytes_raw)
     return asset
 
 
 def get_asset_prices(ticker, parquet_path, data_format=None):
+    """
+    The main function to read data from local parquet file and return data in
+    desired format.
+    :param ticker: string
+    :param parquet_path: string -> path to parquet file on disk
+    :param data_format: string -> can be json, bytes or dict
+    :return: bytes, json or dict
+    """
     df = get_ticker_data_dask(ticker=ticker, file_path=parquet_path)
     asset_message = asset_price_message_from_df(ticker, df)
     if data_format == 'bytes':
@@ -88,7 +128,7 @@ def get_asset_prices(ticker, parquet_path, data_format=None):
     else:
         return message_to_dict(asset_message)
 
-#
+
 # if __name__ == '__main__':
-#     asset_data = get_asset_prices('AAPL', '../data/etl.parquet', 'json')
+#     asset_data = get_asset_prices('AAPL', '../data/etl.parquet')
 #     print(asset_data)
