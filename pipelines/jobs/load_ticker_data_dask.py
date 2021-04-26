32b1e4a266ee2ab9e96ff213049466f6a27adb85
rjdscott <rob@rjdscott.com>
2021-04-25 20:11:25 -0400

    updated dask pipeline

--- pipelines/jobs/load_ticker_data_dask.py
+++ pipelines/jobs/load_ticker_data_dask.py
@@ -2,22 +2,10 @@ import dask
 import dask.dataframe as dd
 import pandas as pd
 from dask.distributed import Client
-
+import os
 import logging
 
 logging.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s', level=logging.INFO)
-
-
-def get_dask_client() -> Client:
-    """
-    Create a distributed dask client
-    :return:
-    """
-    client = Client(n_workers=2, threads_per_worker=2, memory_limit='2GB')
-    try:
-        yield client
-    finally:
-        client.close()
 
 
 def extract(ticker: str, input_file_path: str) -> pd.DataFrame:
@@ -31,19 +19,8 @@ def extract(ticker: str, input_file_path: str) -> pd.DataFrame:
     logging.info(f'extracting data for {ticker}')
 
     # reads csv data from parent data folder
-    df = dd.read_csv(f'{input_file_path}{ticker}.csv')
-    df['ticker'] = ticker
-    return df
-
-
-def transform(df: pd.DataFrame, ticker: str) -> pd.DataFrame:
-    """
-    Transform, clean and enrich original data
-    :param df: Dask DataFrame
-    :param ticker: ticker
-    :return: Pandas Dataframe
-    """
-    logging.info(f'transforming data for {ticker}')
+    full_file_name = f'{os.path.join(input_file_path, ticker)}.csv'
+    df = pd.read_csv(full_file_name)
 
     column_dict = {
         'Adj Close': 'close_adj',
@@ -55,76 +32,42 @@ def transform(df: pd.DataFrame, ticker: str) -> pd.DataFrame:
         'Volume': 'volume'
     }
     df = df.rename(columns=column_dict)
-
+    df['ticker'] = ticker
     df['turnover'] = df.close_adj * df.volume
     return df
 
 
-def load(df: pd.DataFrame, file_path: str, ticker: str) -> None:
+def load(df: pd.DataFrame, file_path: str) -> None:
     """
     Load data into Parquet file (for this example)
     :param df: Dask DataFrame
     :param file_path: path to store data
-    :param ticker: ticker
     :return: None
     """
-    logging.info(f'loading data for {ticker}')
-    df.to_parquet(file_path, engine='pyarrow', write_index=False, append=True)
+    logging.info(f'writing data to {file_path}')
+    df.to_parquet(file_path, engine='pyarrow', write_index=False, partition_on=['ticker'], compression='snappy')
     pass
 
 
 def pipeline(tickers: list,
              parquet_file_path: str,
-             input_file_path: str,
-             in_parallel: bool = True) -> None:
+             input_file_path: str) -> None:
     """
     Pipeline orchestrator function
     :param input_file_path: str
     :param tickers: list
     :param parquet_file_path: string
-    :param in_parallel: bool
     :return: None
     """
-    for ticker in tickers:
+    client = Client(n_workers=2, threads_per_worker=2, memory_limit='2GB')
 
-        if in_parallel:
-            logging.info('pipeline commencing in parallel...')
-            ticker_df = dask.delayed(extract)(ticker=ticker, input_file_path=input_file_path)
-            transform_df = dask.delayed(transform)(df=ticker_df, ticker=ticker)
-            dask.delayed(load)(df=transform_df, file_path=parquet_file_path, ticker=ticker)
-
-        else:
-            logging.info('pipeline commencing in serial...')
-            ticker_df = extract(ticker=ticker, input_file_path=input_file_path)
-            transform_df = transform(df=ticker_df, ticker=ticker)
-            load(df=transform_df, file_path=parquet_file_path, ticker=ticker)
-    pass
-
-
-def pipeline_manager(parquet_file_path: str,
-                     input_file_path: str,
-                     tickers: list,
-                     in_parallel: bool = True) -> None:
-    """
-    Pipeline manager function
-    :param tickers: list
-    :param input_file_path:
-    :param parquet_file_path: str
-    :param in_parallel: bool
-    :return: None
-    """
-
-    if in_parallel:
-        client = Client(n_workers=2, threads_per_worker=2, memory_limit='2GB')
-        try:
-            dask.compute(pipeline(tickers=tickers,
-                                  parquet_file_path=parquet_file_path,
-                                  input_file_path=input_file_path,
-                                  in_parallel=in_parallel))
-        finally:
-            client.close()
-    else:
-        pipeline(tickers=tickers, parquet_file_path=parquet_file, in_parallel=in_parallel)
+    try:
+        logging.info('pipeline commencing in parallel...')
+        ticker_df_list = [dask.delayed(extract)(ticker, input_file_path) for ticker in tickers]
+        ticker_df = dd.from_delayed(ticker_df_list)
+        load(df=ticker_df, file_path=parquet_file_path)
+    finally:
+        client.close()
 
 
 def read_parquet(file_name: str) -> None:
@@ -139,11 +82,10 @@ def read_parquet(file_name: str) -> None:
 
 if __name__ == '__main__':
 
-    parquet_file = '../../data/etl_dask.parquet'
-    input_file_path = '../../data/'
+    parquet_file = os.path.join('../', '../', 'data/', 'all-tickers')
+    input_file_path = os.path.join('../', '../', 'data', 'raw')
     tickers = 'AAPL,AMZN,FB,IBM,MSFT'.split(',')
 
-    pipeline_manager(parquet_file_path=parquet_file,
-                     input_file_path=input_file_path,
-                     tickers=tickers)
-    # read_parquet(parquet_file)
+    pipeline(parquet_file_path=parquet_file,
+             input_file_path=input_file_path,
+             tickers=tickers)
